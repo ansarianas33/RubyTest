0ecd26c4b1150ca95e76f771ddf92110fdfe76b8
microniche333 <70558858+microniche333@users.noreply.github.com>
2023-03-31 02:29:48 +0530

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def send_email
+    email = params[:email]
+    subject = params[:subject]
+    message = params[:message]
+    ApplicationMailer.send_email(email, subject, message).deliver_now
+    render json: { message: 'Email sent successfully!' }
+  rescue StandardError => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
 end
