8d347db049aeb42bece8190b3bb28e5e085ed90d
microniche333 <70558858+microniche333@users.noreply.github.com>
2023-03-31 02:28:30 +0530

    Update application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
+  def send_email(email, subject, message)
+    @message = message
+    mail(to: email, subject: subject)
+  end
 end
